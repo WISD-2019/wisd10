e0a27c0471bd05b12f8d4eca56dbe11ff4096978
Yu Qin <zxas5820@yahoo.com.tw>
2019-12-04 17:01:40 +0800

    a2

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,6 @@ Route::get('/aa/a1', function () {
     return view('aa.a1');
 });
 
-//Route::get('/aa/a2', function () {
-//    return view('aa.a2');
-//});
+Route::get('/aa/a2', function () {
+    return view('aa.a2');
+});
